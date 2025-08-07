a1602503a8791fa3a894e9eb42aba28f5716a8e0
maria eloize <152316133+anethayne@users.noreply.github.com>
2025-08-07 16:30:06 -0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 
         <!-- Container para abrigar o rodapé do sistema -->
         <div class="container-fluid text-center bg-dark text-light p-2 fixed-bottom">
-            Sistema desenvolvido nas aulas de Programação Web I do IFPR Telêmaco Borba
+            Sistema desenvolvido nas aulas de Programação Web I e Banco de Dados do IFPR Telêmaco Borba
         </div>  
 
     </body>
