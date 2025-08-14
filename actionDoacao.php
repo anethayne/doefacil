67bd2d536f63f9374f12adb5141efa91b21403b1
maria eloize <152316133+anethayne@users.noreply.github.com>
2025-08-14 15:23:53 -0300

    kk

---
+++ actionDoacao.php
@@ -0,0 +1,173 @@
+<!-- Inclui o header.php -->
+<?php include "header.php" ?>
+
+    <div class="container mt-3 mb-3">
+
+        <?php
+
+            //Verifica o método de requisição do servidor
+            if($_SERVER["REQUEST_METHOD"] == "POST"){
+                //Bloco para declaração de variáveis
+                $fotoProduto = $nomeProduto = $descricaoProduto = $categoriasProduto = $dataValidadeProduto = $quantidadeProduto = "";
+
+                //Variável booleana para controle de erros de preenchimento
+                $erroPreenchimento = false;
+
+                
+
+                //Validação do campo nome
+                //Utiliza a função empty() para verificar se o campo está vazio
+                if(empty($_POST["nome"])){
+                    echo "<div class='alert alert-warning text-center'>O campo <strong>NOME</strong> é obrigatório!</div>";
+                    $erroPreenchimento = true;
+                }
+                else{
+                    //Armazena valor do formulário na variável
+                    $nomeProduto = filtrar_entrada($_POST["nomeProduto"]);
+                }
+
+                //Validação do campo descricao
+                //Utiliza a função empty() para verificar se o campo está vazio
+                if(empty($_POST["descricao"])){
+                    echo "<div class='alert alert-warning text-center'>O campo <strong>DESCRIÇÃO</strong> é obrigatório!</div>";
+                    $erroPreenchimento = true;
+                }
+                else{
+                    //Armazena valor do formulário na variável
+                    $descricao = filtrar_entrada($_POST["descricao"]);
+                }
+
+                //Validação do campo dataValidade
+                //Utiliza a função empty() para verificar se o campo está vazio
+                if(empty($_POST["dataValidade"])){
+                    echo "<div class='alert alert-warning text-center'>O campo <strong>DATA DE VALIDADE</strong> é obrigatório!</div>";
+                    $erroPreenchimento = true;
+                }
+                else{
+                    //Armazena valor do formulário na variável
+                    $dataValidade = filtrar_entrada($_POST["dataValidade"]);
+
+                    //Aplicar a função strlen() para verificar o comprimento da string da dataValidade
+                    if(strlen($dataValidade) == 10){
+
+                        //Aplicar a função substr() para gerar substrings para armazenar dia, mês e ano da validade do produto
+                        $diaValidadeProduto = substr($dataValidade, 8, 2);
+                        $mesValidadeProduto = substr($dataValidade, 5, 2);
+                        $anoValidadeProduto = substr($dataValidade, 0, 4);
+                    }
+                }
+                //Validação do campo categoria
+                //Utiliza a função empty() para verificar se o campo está vazio
+                if(empty($_POST["categoria"])){
+                    echo "<div class='alert alert-warning text-center'>O campo <strong>CATEGORIA</strong> é obrigatório!</div>";
+                    $erroPreenchimento = true;
+                }
+                else{
+                    //Armazena valor do formulário na variável
+                    $categoria = filtrar_entrada($_POST["categoria"]);
+                }
+
+                //Início da validação da url
+                $diretorio    = "img/"; //Define para qual diretório as imagens serão movidas
+                $url  = $diretorio . basename($_FILES['url']['name']); //img/joaozinho.jpg
+                $tipoDaImagem = strtolower(pathinfo($url, PATHINFO_EXTENSION)); //Pega o tipo do arquivo em letras minúsculas
+                $erroUpload   = false; //Variável para controle do upload da foto
+
+                //Verifica se o tamanho do arquivo é DIFERENTE DE ZERO
+                if($_FILES['fotoProduto']['size'] != 0){
+
+                    //Verifica se o tamanho do arquivo é maior do que 5 MegaBytes (MB) - Medida em Bytes
+                    if($_FILES['fotoProduto']['size'] > 5000000){
+                        echo "<div class='alert alert-warning text-center'>A <strong>FOTO</strong> deve ter tamanho máximo de 5MB!</div>";
+                        $erroUpload = true;
+                    }
+
+                    //Verifica se a foto está nos formatos JPG, JPEG, PNG ou WEBP
+                    if($tipoDaImagem != "jpg" && $tipoDaImagem != "jpeg" && $tipoDaImagem != "png" && $tipoDaImagem != "webp"){
+                        echo "<div class='alert alert-warning text-center'>A <strong>FOTO</strong> deve estar nos formatos JPG, JPEG, PNG ou WEBP</div>";
+                        $erroUpload = true;
+                    }
+
+                    //Verifica se a imagem foi movida para o diretório IMG, utilizando a função move_uploaded_file
+                    if(!move_uploaded_file($_FILES['fotoProduto']['tmp_name'], $url)){
+                        echo "<div class='alert alert-danger text-center'>Erro ao tentar mover a <strong>FOTO</strong> para o diretório $diretorio!</div>";
+                        $erroUpload = true;
+                    }
+
+                }
+                else{
+                    echo "<div class='alert alert-warning text-center'>O campo <strong>FOTO</strong> é obrigatório!</div>";
+                    $erroUpload = true;
+                }
+
+                //Se não houver erro de preenchimento, exibe alerta de sucesso e uma tabela com os dados informados
+                if(!$erroPreenchimento && !$erroUpload){
+
+                    //Cria uma variável para armazenar a QUERY para realizar a inserção dos dados do produto na tabela Produtos
+                    $inserirProduto = "INSERT INTO Produtos (url, nome, descricao, categorias, data_validade, quantidade) VALUES ('$url', '$nome', '$descricao', '$categoria', '$dataValidade', '$quantidade')";
+
+                    //Inclui o arquivo de conexão com o Banco de Dados
+                    include("conexaoBD.php");
+
+                    //Se conseguir executar a query para inserção, exibe alerta de sucesso e a tabela com os dados informados
+                    if(mysqli_query($conn, $inserirProduto)){
+
+                        echo "<div class='alert alert-success text-center'><strong>Produto</strong> cadastrado(a) com sucesso!</div>";
+                        echo "
+                            <div class='container mt-3'>
+                                <div class='container mt-3 text-center'>
+                                    <img src='$url' style='width:150px;' title='Foto de $nome'>
+                                </div>
+                                <table class='table'>
+                                    <tr>
+                                        <th>NOME</th>
+                                        <td>$nome</td>
+                                    </tr>
+                                    <tr>
+                                        <th>DESCRIÇÃO DO PRODUTO</th>
+                                        <td>$descricao</td>
+                                    </tr>
+                                    <tr>
+                                        <th>DATA DE VALIDADE DO PRODUTO</th>
+                                        <td>$dataValidade</td>
+                                    </tr>
+                                    <tr>
+                                        <th>CATEGORIA DO PRODUTO</th>
+                                        <td>$categoria</td>
+                                    </tr>
+                                    <tr>
+                                        <th>QUANTIDADE DO PRODUTO</th>
+                                        <td>$quantidade</td>
+                                    </tr>
+                                    
+                                    
+                                    </table>
+                            </div>
+                        ";
+                        mysqli_close($conn); //Essa função encerra a conexão com o Banco de Dados
+                    }
+                    else{
+                        echo "<div class='alert alert-danger text-center'>Erro ao tentar cadastrar <strong>Produto</strong> no Banco de Dados $database!</div>" . mysqli_error($conn);
+                    }
+                }
+            }
+            else{
+                //Redireciona o usuário para o formProduto.php
+                header("location:formProduto.php");
+            }
+
+            //Função para filtrar entrada de dados
+            function filtrar_entrada($dado){
+                $dado = trim($dado); //Remove espaços desnecessários
+                $dado = stripslashes($dado); //Remove barras invertidas
+                $dado = htmlspecialchars($dado); // Converte caracteres especiais em entidades HTML
+
+                //Retorna o dado após filtrado
+                return($dado);
+            }
+        ?>
+
+    </div>
+
+<!-- Inclui o footer.php -->
+<?php include "footer.php" ?>
---
+++ exibirLogo.php
@@ -0,0 +1,9 @@
+<!-- Header-->
+        <header class="bg-dark py-5">
+            <div class="container px-4 px-lg-5 my-5">
+                <div class="text-center text-white">
+                    <img src="img/doefacil.jpg" style="width:200px;" title="Retornar para a Página Inicial">
+                    <p class="lead fw-normal text-white-50 mb-0">Aqui ficará o slogan do projeto Doe Fácil! =)</p>
+                </div>
+            </div>
+        </header>
