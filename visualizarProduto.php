e5bb46ade0c8422e546448af8acf6e08ef042284
maria eloize <152316133+anethayne@users.noreply.github.com>
2025-08-21 14:28:09 -0300

    Update visualizarProduto.php

--- visualizarProduto.php
+++ visualizarProduto.php
@@ -5,107 +5,100 @@
     <div class='row text-center'>
 
     <?php
-
         //Verifica se há recebimento de parâmetro via método GET
         if(isset($_GET['idProduto'])){
             $idProduto = $_GET['idProduto'];
 
-            //Inclui um arquivo de conexão com o Banco de Dados 
+            //Inclui conexão
             include "conexaoBD.php";
 
-            $exibirProduto = "SELECT * FROM Produtos WHERE idProduto = $idProduto";
-
-            $res = mysqli_query($conn, $exibirProduto); //Executa essa QUERY
-            $totalProdutos = mysqli_num_rows($res); //Retorna a quantidade de registro
+            $exibirDoacao = "SELECT * FROM Produtos WHERE id_produtos = $idProduto";
+            $res = mysqli_query($conn, $exibirDoacao); 
+            $totalDoacoes = mysqli_num_rows($res); 
             
-            if($totalProdutos > 0){
+            if($totalDoacoes > 0){
                 
                 if($registro = mysqli_fetch_assoc($res)){
-                    $idProduto  = $registro['idProduto'];
-                    $fotoProduto = $registro['fotoProduto'];
-                    $nomeProduto = $registro['nomeProduto'];
-                    $descricaoProduto = $registro['descricaoProduto'];
-                    $valorProduto = $registro['valorProduto'];
-                    $statusProduto = $registro['statusProduto'];
-        
-                    ?>
+                    $idDoacao    = $registro['id_produtos'];
+                    $urls        = $registro['url']; // pode ter várias separadas por vírgula
+                    $nome        = $registro['nome'];
+                    $descricao   = $registro['descricao'];
+                    $categoria   = $registro['categorias'];
+                    $dataVal     = $registro['data_validade'];
+                    $quantidade  = $registro['quantidade'];
 
+                    // transforma em array de imagens
+                    $imagens = explode(",", $urls);
+                    ?>
 
                         <div class="d-flex justify-content-center mb-3">
 
-                            <div class="card" style="width:30%; border-style:none;">
+                            <div class="card" style="width:40%; border-style:none;">
                                             
                                 <!-- Carousel -->
-                                <div id="Produto" class="carousel slide" data-bs-ride="carousel" >
+                                <div id="carousel<?php echo $idDoacao?>" class="carousel slide" data-bs-ride="carousel">
 
-                                    <!-- Indicators/dots -->
-                                    <div class="carousel-indicators">
-                                        <button type="button" data-bs-target="#Produto" data-bs-slide-to="0" class="active"></button>
-                                        <button type="button" data-bs-target="#Produto" data-bs-slide-to="1"></button>
-                                        <button type="button" data-bs-target="#Produto" data-bs-slide-to="2"></button>
-                                    </div>
-
-                                    <!-- The slideshow/carousel -->
+                                    <!-- Slideshow -->
                                     <div class="carousel-inner">
-                                        <div class="carousel-item active">
-                                            <img src="<?php echo $fotoProduto?>" alt="<?php echo $nomeProduto?>" class="d-block" style="width:100%">
-                                        </div>
-                                        <div class="carousel-item">
-                                            <img src="<?php echo $fotoProduto?>" alt="<?php echo $nomeProduto?>" class="d-block" style="width:100%">
-                                        </div>
-                                        <div class="carousel-item">
-                                            <img src="<?php echo $fotoProduto?>" alt="<?php echo $nomeProduto?>" class="d-block" style="width:100%">
-                                        </div>
+                                        <?php
+                                            $active = "active";
+                                            foreach($imagens as $img){
+                                                echo "
+                                                    <div class='carousel-item $active'>
+                                                        <img src='$img' alt='$nome' class='d-block w-100'>
+                                                    </div>
+                                                ";
+                                                $active = "";
+                                            }
+                                        ?>
                                     </div>
 
-                                    <!-- Left and right controls/icons -->
-                                    <button class="carousel-control-prev" type="button" data-bs-target="#Produto" data-bs-slide="prev">
+                                    <!-- Controls -->
+                                    <button class="carousel-control-prev" type="button" data-bs-target="#carousel<?php echo $idDoacao?>" data-bs-slide="prev">
                                         <span class="carousel-control-prev-icon"></span>
                                     </button>
-                                    <button class="carousel-control-next" type="button" data-bs-target="#Produto" data-bs-slide="next">
+                                    <button class="carousel-control-next" type="button" data-bs-target="#carousel<?php echo $idDoacao?>" data-bs-slide="next">
                                         <span class="carousel-control-next-icon"></span>
                                     </button>
                                 </div>
                                 
-                                <div class="card-body">
-                                    <h4 class="card-title"><b><?php echo $nomeProduto?></b></h4>
-                                    <p class="card-text"><?php echo $descricaoProduto?></p>
-                                    <p class="card-text">Valor: <b><?php echo $valorProduto?></b></p>
-                                    <div class="card bg-light">
+                                <div class="card-body text-center">
+                                    <h4 class="card-title"><b><?php echo $nome?></b></h4>
+                                    <p class="card-text"><?php echo $descricao?></p>
+                                    <p class="card-text"><b>Categoria:</b> <?php echo $categoria?></p>
+                                    <p class="card-text"><b>Validade:</b> <?php echo $dataVal?></p>
+                                    <p class="card-text"><b>Quantidade:</b> <?php echo $quantidade?></p>
+                                    
+                                    <div class="card bg-light mt-3">
                                         <div class="card-body">
-                                            <a href="#" title="Comprar">
+                                            <a href="#" title="Solicitar doação">
                                                 <button class="btn btn-outline-success">
-                                                    <i class="bi bi-clipboard-plus" style="font-size:16pt;"></i>
-                                                    Comprar
+                                                    <i class="bi bi-hand-thumbs-up" style="font-size:16pt;"></i>
+                                                    Solicitar Doação
                                                 </button>
                                             </a>
                                         </div>
-                                        <br>
                                     </div>
                                 </div>
 
                             </div>
 
-                    </div>
-                <?php
-                
+                        </div>
+                    <?php
                 }
                 else{
-                    echo "<div class='alert alert-warning text-center'>Produto não localizado</div>";
+                    echo "<div class='alert alert-warning text-center'>Doação não localizada</div>";
                 }
-            echo "<div>";
             }
             else{
-                echo "<div class='alert alert-warning text-center'>Não foi possível carregar o produto</div>";
+                echo "<div class='alert alert-warning text-center'>Não foi possível carregar a doação</div>";
             }
         }
-
     ?>
-
-        
 
     </div>
 
 </div>
+
 
 <?php include "footer.php" ?>
