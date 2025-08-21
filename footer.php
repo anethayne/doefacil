e55af3ce7d9e086bf7819e4b2d5c08fea6e0ea0a
amanda <amandavcochinski@gmail.com>
2025-08-21 17:39:13 -0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <!-- Footer-->
         <footer class="py-2 fixed-bottom" style="background-color: #b95b5b; height: 50px;">
-            <div class="container"><p class="m-0 text-center text-light">Sistema Web desenvolvido por Ane, Amanda e Estefany - IFPR Campus Telêmaco Borba</p></div>
+            <div class="container"><p class="m-1 text-center text-light">Sistema Web desenvolvido por Ane, Amanda e Estefany - IFPR Campus Telêmaco Borba</p></div>
         </footer>
         <!-- Bootstrap core JS-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
