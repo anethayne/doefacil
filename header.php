1e50e4bb40fb19601e3952dd010811d551162ef0
amanda <amandavcochinski@gmail.com>
2025-08-21 14:22:43 -0300

    vv

--- header.php
+++ header.php
@@ -39,53 +39,45 @@
     </head>
 
     <body>
+
         <!-- Navigation -->
-<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #b95b5bff;">
-    <div class="container px-4 px-lg-5">
+    <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #b95b5bff;">
+        <div class="container px-4 px-lg-5">
 
-        <a class="navbar-brand" href="index.php">Compartilhe amor em forma de alimento! <i class="bi bi-emoji-smile"></i>
-                    <img src="img/logodoefacil.png" style="width:80px;" title="Retornar para a Página Inicial">
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
-                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="index.php">Home</a></li>
-                        <li class="nav-item"><a class="nav-link" aria-current="page" href="formDoacao.php">Efetuar Doação</a></li>
-                        <li class="nav-item"><a class="nav-link" aria-current="page" href="formProduto.php">Cadastrar Produto</a></li>
-                        <li class="nav-item"><a class="nav-link" href="formLogin.php">Login</a></li>
-                        <!-- 
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Shop</a>
-                            
-                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a class="dropdown-item" href="#!">All Products</a></li>
-                                <li><hr class="dropdown-divider" /></li>
-                                <li><a class="dropdown-item" href="#!">Popular Items</a></li>
-                                <li><a class="dropdown-item" href="#!">New Arrivals</a></li>
-                            </ul>
+            <!-- Logo e slogan lado a lado -->
+            <a class="navbar-brand d-flex align-items-center" href="index.php">
+                <!-- Logo à esquerda -->
+                <img src="img/logodoefacil.png" alt="Logo DoeFácil" style="width:80px;" title="Retornar para a Página Inicial" class="me-2">
+                
+                <!-- Texto à direita da logo -->
+                <span>Compartilhe amor em forma de alimento! <i class="bi bi-emoji-smile"></i></span>
+            </a>
 
-        <!-- Logo e slogan lado a lado -->
-        <a class="navbar-brand d-flex align-items-center" href="index.php">
-            <!-- Logo à esquerda -->
-            <img src="img/logodoefacil.png" alt="Logo DoeFácil" style="width:80px;" title="Retornar para a Página Inicial" class="me-2">
-            
-            <!-- Texto à direita da logo -->
-            <span>Compartilhe amor em forma de alimento! <i class="bi bi-emoji-smile"></i></span>
-        </a>
+            <!-- Botão do menu (mobile) -->
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" 
+                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-        <!-- Botão do menu (mobile) -->
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" 
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+            <!-- Itens de navegação -->
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+                    <li class="nav-item"><a class="nav-link active" aria-current="page" href="index.php">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="formDoacao.php">Efetuar Doação</a></li>
+                    <li class="nav-item"><a class="nav-link" href="formLogin.php">Login</a></li>
+                </ul>
 
-        <!-- Itens de navegação -->
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
-                <li class="nav-item"><a class="nav-link" aria-current="page" href="index.php">Home</a></li>
-                <li class="nav-item"><a class="nav-link" href="formDoacao.php">Efetuar Doação</a></li>
-                <li class="nav-item"><a class="nav-link" href="formLogin.php">Login</a></li>
-            </ul>
+                <!-- Botão do carrinho -->
+                <form class="d-flex">
+                    <button class="btn btn-outline-light" type="submit">
+                        <i class="bi-cart-fill me-1"></i>
+                        Carrinho
+                        <span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
+                    </button>
+                </form>
+            </div>
+        </div>
+    </nav>
 
                 <script>
         // Pega o arquivo da URL atual (ex: index.php)
@@ -105,16 +97,4 @@
             }
         });
     </script>
-
-            <!-- Botão do carrinho -->
-            <form class="d-flex">
-                <button class="btn btn-outline-light" type="submit">
-                    <i class="bi-cart-fill me-1"></i>
-                    Carrinho
-                    <span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
-                </button>
-            </form>
-        </div>
-    </div>
-</nav>
         
